facad9b068e926b781232afa114e64d4954a74cd
STHEXA <ti.syu.s@gmail.com>
2020-10-25 14:00:02 +0900

    真偽値の確認

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ console.log("5" + 3);
 console.log(parseInt("5", 10) + 3);
 
 console.log(parseInt("hello", 10) + 3);
+
+const price = 1200;
+// console.log(price > 1000);
+// console.log(price < 1000);
+// console.log(price >= 1000);
+// console.log(price <= 1000);
+// console.log(price === 1000);
+// console.log(price !== 1000);
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
