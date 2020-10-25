e6f7845d5e7a78d8546da1de3b73b338080d7f48
STHEXA <ti.syu.s@gmail.com>
2020-10-25 13:49:26 +0900

    変数を使った計算

--- js/main.js
+++ js/main.js
@@ -1,31 +1,20 @@
 'use strict';
 
-// コメント
-
-// console.log('Hello world from main.js!');
-// console.log('Hello world from main.js!');
-// console.log('it\'s me!');
-// console.log("it's me!");
-// console.log('hel\nlo wor\tld');
-// console.log('hello' + 'world');
-
-// console.log(10 + 3);
-// console.log(10 - 3);
-// console.log(10 * 3);
-// console.log(10 / 3);
-// console.log(10 % 3);
-// console.log(10 ** 3);
-
-// console.log(2 + 10 * 3);
-// console.log((2 + 10) * 3);
-
-//定数 const
 // 変数 let
 
-let price = 150;
-console.log(price * 140);
-console.log(price * 160);
+let price = 500;
 
-price = 170;
-console.log(price * 140);
-console.log(price * 160);
+// price = price + 100
+price += 100;
+
+// price = price * 2
+price *= 2;
+
+// price = price + 1;
+// price += 1;
+price++;
+
+// price -= 1;
+price--;
+
+console.log(price);
