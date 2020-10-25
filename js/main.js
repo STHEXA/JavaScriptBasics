b6de822b193a5fcf89f78cc8fa2cada60a77160b
STHEXA <ti.syu.s@gmail.com>
2020-10-25 13:42:09 +0900

    init commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// コメント
+
+// console.log('Hello world from main.js!');
+// console.log('Hello world from main.js!');
+// console.log('it\'s me!');
+// console.log("it's me!");
+// console.log('hel\nlo wor\tld');
+// console.log('hello' + 'world');
+
+// console.log(10 + 3);
+// console.log(10 - 3);
+// console.log(10 * 3);
+// console.log(10 / 3);
+// console.log(10 % 3);
+// console.log(10 ** 3);
+
+// console.log(2 + 10 * 3);
+// console.log((2 + 10) * 3);
+
+//定数 const
+// 変数 let
+
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+console.log(price * 140);
+console.log(price * 160);
