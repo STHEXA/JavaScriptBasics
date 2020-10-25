cdc56b851e7e7f8d9ae84a6f3becd2428b0caf3c
STHEXA <ti.syu.s@gmail.com>
2020-10-25 14:06:24 +0900

    条件演算子

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const score = 40;
-if(score >= 80){
-  console.log('great');
-} else if(score >= 60) {
-  console.log('good');
-} else {
-  console.log('ok');
-}
+const score = 85;
+// if(score >= 80){
+//   console.log('great');
+// } else if(score >= 60) {
+//   console.log('good');
+// } else {
+//   console.log('ok');
+// }
+
+score >= 80　? console.log('great') : console.log('ok');
